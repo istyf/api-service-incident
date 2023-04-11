edf4758ab00d127b30ca129efcf32b2eda83ff16
lsjolinder <linus.sjolinder@b3.se>
2023-04-11 11:33:29 +0200

    Wrap only the jsonobject in objectmapper

--- src/test/java/se/sundsvall/incident/integration/lifebuoy/model/RequestWrapperTest.java
+++ src/test/java/se/sundsvall/incident/integration/lifebuoy/model/RequestWrapperTest.java
@@ -1,18 +1,20 @@
 package se.sundsvall.incident.integration.lifebuoy.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class RequestWrapperTest {
 
     @Test
     void testBuilderAndSerialization() throws JsonProcessingException {
 
+        var mapper = new ObjectMapper();
         LifeBuoyRequestWrapper wrapper = LifeBuoyRequestWrapper.builder()
                 .withApiKey("someApiKey")
-                .withErrandJsonString(LifebuoyRequest.builder().build())
+                .withErrandJsonString(mapper.writeValueAsString(LifebuoyRequest.builder().build()))
                 .build();
 
 
