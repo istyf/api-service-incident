995f9b7b10d607a5379a9432acb92de95437bd08
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-12-21 09:36:01 +0100

    UF-6363: Cleanup dependency version

--- src/main/java/se/sundsvall/incident/integration/db/repository/IncidentRepository.java
+++ src/main/java/se/sundsvall/incident/integration/db/repository/IncidentRepository.java
@@ -4,13 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import se.sundsvall.incident.integration.db.entity.IncidentEntity;
-
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import se.sundsvall.incident.integration.db.entity.IncidentEntity;
 
 @CircuitBreaker(name = "incidentRepository")
 public interface IncidentRepository extends JpaRepository<IncidentEntity, String> {
 
 	Optional<IncidentEntity> findIncidentEntityByExternalCaseId(final String externalCaseId);
-
 }
